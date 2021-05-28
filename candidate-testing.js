ac8fb7fe0cd336f78ded542e5f59b5e130ba5caa
Hope Ciuffa <>
2021-05-28 12:04:53 +0000

    part 2 code update

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,7 +8,7 @@ let candidateName = "";
 let question = "Who was the first American woman in space?";
 let correctAnswer = "SALLY RIDE";
 let candidateAnswer = "";
-let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters?", "(5 + 3)/2 * 10 = ?", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?", "What is the minimum crew size for the ISS?"];
+let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
 let correctAnswers = ["SALLY RIDE", "TRUE", "40","TRAJECTORY", "3"];
 let candidateAnswers = "";
 
@@ -17,29 +17,28 @@ function askForName() {
   // TODO 1.1b: Ask for candidate's name //
   candidateName = input.question("Enter your Name: ");
   console.log("Welcome, " + candidateName);
-  
-
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  candidateAnswer = input.question(question);
-  candidateAnswer = candidateAnswer.toUpperCase();
-
+  //PART 1: candidateAnswer = input.question(question);
+  //candidateAnswer = candidateAnswer.toUpperCase();
+  for (i = 0; i < questions.length; i++) {
+    candidateAnswers = input.question(questions[i]);
+    candidateAnswers = candidateAnswers.toUpperCase();
+    console.log(candidateAnswers);
+    if (candidateAnswers === correctAnswers) {
+      console.log('answer correct');
+    } else {
+      console.log('answer incorrect');
+    }  
+  }
 }
   //candidateAnswers = input.question(questions);
   //candidateAnswers = candidateAnswers.toUpperCase();
 function gradeQuiz(candidateAnswers) {
-
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  if (candidateAnswer === "SALLY RIDE") {
-    console.log("You answered correctly");
-  } 
-  else {
-    console.log("Answer is incorrect");
-  }
-
-
+  
   let grade = " ";
   if (candidateAnswer ==="SALLY RIDE") {
     grade = "Test score, Pass";
@@ -53,8 +52,6 @@ function gradeQuiz(candidateAnswers) {
 
 function runProgram() {
   askForName();
-  // TODO 1.1c: Ask for candidate's name //
-  
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
